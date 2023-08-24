e638c945cf76bd918eec29475ae37ad8204dc706
Merdan D <merdan.durdiyev@gmail.com>
2023-08-24 18:12:53 +0500

    Adds categories card to manager dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
   # GET /users/slug/dashboard
   def manager_dashboard
     @restaurant = current_user.restaurant
+    @categories = @restaurant.categories
   end
 end
